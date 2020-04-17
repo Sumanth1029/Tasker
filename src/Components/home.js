0cfd1b2f55f00afab344dc6e9f51707eff1bd762
Sumanth Hegde <sumanthhegde1029@gmail.com>
2020-04-17 16:53:22 +0530

    split tasksbar and added cards

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,18 +1,16 @@
 import React, { Component, Fragment } from "react";
-import Tasksbar from "./tasksbar"
-import Title from "./title"
-
-
+import Tasksbar from "./tasksbar";
+import Title from "./title";
 
 export default class Home extends Component {
- 
-
   render() {
     return (
       <Fragment>
-          <Title/>
-          <Tasksbar/>
-        
+        <Title />
+        <Tasksbar />
+        {/* <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab> */}
       </Fragment>
     );
   }
