e37188f8aa74358f157540c9deafa680b250f96f
Sumanth Hegde <sumanthhegde1029@gmail.com>
2020-04-21 11:11:57 +0530

    Updates

---
+++ precache-manifest.282ac3d48bd917314f9671364a2f86f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d19b938d6dacafce785340bb2548d21",
+    "url": "/Sumanth1029/Tasker_React/index.html"
+  },
+  {
+    "revision": "6403941f5c7b9afc344c",
+    "url": "/Sumanth1029/Tasker_React/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "bce6cfbffd196491a904",
+    "url": "/Sumanth1029/Tasker_React/static/css/main.54684971.chunk.css"
+  },
+  {
+    "revision": "6403941f5c7b9afc344c",
+    "url": "/Sumanth1029/Tasker_React/static/js/2.be13ecdd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Sumanth1029/Tasker_React/static/js/2.be13ecdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce6cfbffd196491a904",
+    "url": "/Sumanth1029/Tasker_React/static/js/main.8b9fe9a3.chunk.js"
+  },
+  {
+    "revision": "bb9cc8b7c6e5f34c4802",
+    "url": "/Sumanth1029/Tasker_React/static/js/runtime-main.02e7c54d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sumanth1029/Tasker_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
